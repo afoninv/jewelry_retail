adb717caa1ab53f5bec6eb7fc46a2e263f7dd553
jewelryretail <jewelryretail@mybox.(none)>
2011-10-04 05:05:45 +0200

    changes to urls.py (removed unused urls)

---
+++ urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls.defaults import patterns, include, url
+from jewelry_retail.views import mainpage, catalogue, catalogue_view, catalogue_search
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'jewelry_retail.views.home', name='home'),
+    # url(r'^jewelry_retail/', include('jewelry_retail.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+
+    ('^$', mainpage),
+    ('^catalogue/$', catalogue),
+    ('^catalogue/search/$', catalogue_search),
+    ('^catalogue/(?P<j_type>\w+)/$', catalogue_view),
+    ('^catalogue/(?P<j_type>\w+)/(?P<j_id>\d+)/$', catalogue_view),
+
+#    (r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'c:/python26/jewelry_retail/images/'}),
+
+)
