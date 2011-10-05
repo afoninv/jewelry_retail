7a178621e30def779130d8603734974c0d061b45
afoninv <afoninv@mail.ru>
2011-10-05 08:33:46 +0200

    v 0.1.4
    tweaked search a bit

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 ﻿from django import forms
-from jewelry_retail.data_storage.models import JewelryType,Gem
+from jewelry_retail.data_storage.models import JewelryType,Gem, Metal, Gender
 
 class JRAdvancedSearchForm(forms.Form):
     j_type = forms.ModelChoiceField(queryset=JewelryType.objects.all())
     gem = forms.ModelChoiceField(queryset=Gem.objects.all())
     price_min = forms.IntegerField(min_value=0, required=False)
     price_max = forms.IntegerField(min_value=0, required=False)
-
+    metal = forms.ModelChoiceField(queryset=Metal.objects.all())
+    gender = forms.ModelChoiceField(queryset=Gender.objects.all())
 
     def clean(self):
         price_min=self.cleaned_data.get('price_min', None)
