7dbda8f2746c85b1b0e038c6647a9571270417cb
unknown <Admin@.(none)>
2011-10-05 15:12:37 +0400

    v 0.2.3
    - bugfix

--- jr_cart/views.py
+++ jr_cart/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext
 from jewelry_retail.data_storage.models import Article, Suite
 
 def cart(request):
-    request.session['cart'] = request.session.get('cart', [])
+    request.session['cart'] = request.session.get('cart', []) or []
     return render_to_response("jr_cart.html", {'items': request.session['cart']}, context_instance=RequestContext(request))
 
 def cart_add(request, j_type=None, j_id=None):
@@ -18,7 +18,7 @@ def cart_add(request, j_type=None, j_id=None):
         item = Article.objects.get(id=j_id)
         item.j_type_eng = unicode(item.j_type.name_eng)
 
-    items = request.session.get('cart') or []
+    items = request.session.get('cart', []) or []
     items.append(item)
     request.session['cart'] = items
 
