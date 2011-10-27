72afcdba98bf3f7b4277c2e549f34bf371ef978e
unknown <Admin@.(none)>
2011-10-27 09:43:02 +0400

    v 0.5.6.2
    - ...aaand bugfix again

--- jr_cart/templatetags/jr_cart.py
+++ jr_cart/templatetags/jr_cart.py
@@ -8,7 +8,7 @@ def cart_summary(request):
     return {"cart_item_count": cart_item_count}
 
 def cart_list(request):
-    return {'cart_list': request.session.get('cart') or [])
+    return {'cart_list': request.session.get('cart') or []}
 
 register.inclusion_tag("jr_cart_summary.html")(cart_summary)
 register.inclusion_tag("jr_cart_list.html")(cart_list)
